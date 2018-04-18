d5354c7bc072bd0ed75093cd3a681f116b24dfcd
Brian Mancini <bmancini@gmail.com>
2018-04-18 19:16:20 -0400

    Adding HitBTC client to export

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ module.exports = {
   Bitstamp: require("./exchanges/bitstamp-client"),
   Bittrex: require("./exchanges/bittrex-client"),
   GDAX: require("./exchanges/gdax-client"),
+  HitBTC: require("./exchanges/hitbtc-client"),
   Poloniex: require("./exchanges/poloniex-client"),
 };
