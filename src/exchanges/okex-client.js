bf361822d57ff325a905048cf3c37f4a17666264
Brian Mancini <bmancini@gmail.com>
2018-05-22 15:17:32 -0400

    Issue #5: fixing OKEx disconnect on large subscribe

--- src/exchanges/okex-client.js
+++ src/exchanges/okex-client.js
@@ -1,10 +1,12 @@
+const semaphore = require("semaphore");
 const BasicClient = require("../basic-client");
 const Trade = require("../trade");
 
 class OKExClient extends BasicClient {
   constructor() {
-    super("wss://real.okex.com:10441/websocket", "HitBTC");
-    this._pingInterval = setInterval(this._sendPing.bind(this), 5000);
+    super("wss://real.okex.com:10441/websocket", "OKEx");
+    this._pingInterval = setInterval(this._sendPing.bind(this), 30000);
+    this._sem = semaphore(10);
   }
 
   _sendPing() {
@@ -14,13 +16,15 @@ class OKExClient extends BasicClient {
   }
 
   _sendSubscribe(remote_id) {
-    let [base, quote] = remote_id.split("_");
-    this._wss.send(
-      JSON.stringify({
-        event: "addChannel",
-        parameters: { base, binary: "0", product: "spot", quote, type: "deal" },
-      })
-    );
+    this._sem.take(() => {
+      let [base, quote] = remote_id.split("_");
+      this._wss.send(
+        JSON.stringify({
+          event: "addChannel",
+          parameters: { base, binary: "0", product: "spot", quote, type: "deal" },
+        })
+      );
+    });
   }
 
   _sendUnsubscribe(remote_id) {
@@ -34,11 +38,35 @@ class OKExClient extends BasicClient {
   }
 
   _onMessage(raw) {
+    /*
+    [{ base: '1st',
+      binary: 0,
+      channel: 'addChannel',
+      data: { result: true },
+      product: 'spot',
+      quote: 'btc',
+      type: 'deal' },
+    { base: '1st',
+      binary: 0,
+      data:
+      [ { amount: '818.619',
+          side: 1,
+          createdDate: 1527013680457,
+          price: '0.00003803',
+          id: 4979071 },
+      ],
+      product: 'spot',
+      quote: 'btc',
+      type: 'deal' }]
+    */
     let msgs = JSON.parse(raw);
     if (!Array.isArray(msgs)) return;
     for (let msg of msgs) {
       if (msg.product === "spot" && msg.type === "deal") {
-        if (!Array.isArray(msg.data)) return; // handle confirmation
+        if (msg.data.result) {
+          this._sem.leave();
+          continue;
+        }
         let { base, quote } = msg;
         let remote_id = `${base}_${quote}`;
         for (let datum of msg.data) {
