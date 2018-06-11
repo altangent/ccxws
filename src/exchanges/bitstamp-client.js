0bc6acaa17897a66288a544878fe52e59d53609b
Brian Mancini <bmancini@gmail.com>
2018-06-11 18:59:19 -0400

    Prototype for bistamp l2/l3 orderbooks

--- src/exchanges/bitstamp-client.js
+++ src/exchanges/bitstamp-client.js
@@ -7,26 +7,82 @@ class BitstampClient extends EventEmitter {
   constructor() {
     super();
     this._name = "Bitstamp";
-    this._subscriptions = new Map();
+    this._tradeSubs = new Map();
+    this._level2SpotSubs = new Map();
+    this._level2UpdateSubs = new Map();
+    this._level3UpdateSubs = new Map();
   }
 
   subscribeTrades(market) {
-    this._connect();
-    let remote_id = market.id;
-    if (!this._subscriptions.has(remote_id)) {
-      winston.info("subscribing to", this._name, remote_id);
-      this._subscriptions.set(remote_id, market);
-      this._sendSubscribe(remote_id);
-    }
+    this._subscribe(
+      market,
+      this._tradeSubs,
+      "subscribing to trades",
+      this._sendSubTrades.bind(this)
+    );
   }
 
   unsubscribeTrades(market) {
-    let remote_id = market.id;
-    if (this._subscriptions.has(remote_id)) {
-      winston.info("unsubscribing from", this._name, remote_id);
-      this._subscriptions.delete(remote_id);
-      this._sendUnsubscribe(remote_id);
-    }
+    this._unsubscribe(
+      market,
+      this._tradeSubs,
+      "unsubscribing from trades",
+      this._sendUnsubTrades.bind(this)
+    );
+  }
+
+  subscribeLevel2Spot(market) {
+    this._subscribe(
+      market,
+      this._level2SpotSubs,
+      "subscribing to level2 spot",
+      this._sendSubLevel2Spot.bind(this)
+    );
+  }
+
+  unsubscribeLevel2Spot(market) {
+    this._unsubscribe(
+      market,
+      this._level2SpotSubs,
+      "unsubscribing from level2 spot",
+      this._sendUnsubLevel2Spot.bind(this)
+    );
+  }
+
+  subscribeLevel2Updates(market) {
+    this._subscribe(
+      market,
+      this._level2UpdateSubs,
+      "subscribing to level2 updates",
+      this._sendSubLevel2Updates.bind(this)
+    );
+  }
+
+  unsubscribeLevel2Updates(market) {
+    this._unsubscribe(
+      market,
+      this._level2UpdateSubs,
+      "unsubscribing from level2 updates",
+      this._sendUnsubLevel2Updates.bind(this)
+    );
+  }
+
+  subscribeLevel3Updates(market) {
+    this._subscribe(
+      market,
+      this._level3UpdateSubs,
+      "subscribing to level3 updates",
+      this._sendSubLevel3Updates.bind(this)
+    );
+  }
+
+  unsubscribeLevel3Updates(market) {
+    this._subscribe(
+      market,
+      this._level3UpdateSubs,
+      "unsubscribing from level3 updates",
+      this._sendUnsubLevel3Updates.bind(this)
+    );
   }
 
   close() {
@@ -45,11 +101,40 @@ class BitstampClient extends EventEmitter {
     }
   }
 
-  _onMessage(remote_id, msg) {
-    let market = this._subscriptions.get(remote_id);
+  _subscribe(market, map, msg, subFn) {
+    this._connect();
+    let remote_id = market.id;
+    if (!map.has(remote_id)) {
+      winston.info(msg, this._name, remote_id);
+      map.set(remote_id, market);
+      subFn(remote_id);
+    }
+  }
 
-    /* trade message format:
+  _unsubscribe(market, map, msg, subFn) {
+    let remote_id = market.id;
+    if (map.has(remote_id)) {
+      winston.info(msg, this._name, remote_id);
+      map.delete(remote_id);
+      subFn(remote_id);
+    }
+  }
 
+  _sendSubTrades(remote_id) {
+    let channelName = remote_id === "btcusd" ? "live_trades" : `live_trades_${remote_id}`;
+    let channel = this._pusher.subscribe(channelName);
+    channel.bind("trade", this._onTrade.bind(this, remote_id));
+  }
+
+  _sendUnsubTrades(remote_id) {
+    let channelName = remote_id === "btcusd" ? "live_trades" : `live_trades_${remote_id}`;
+    this._pusher.unsubscribe(channelName);
+  }
+
+  _onTrade(remote_id, msg) {
+    let market = this._tradeSubs.get(remote_id);
+
+    /*
     { amount: 0.363,
       buy_order_id: 1347930302,
       sell_order_id: 1347930276,
@@ -59,7 +144,6 @@ class BitstampClient extends EventEmitter {
       price: 8094.97,
       type: 0,
       id: 62696598 }
-
     */
 
     let trade = new Trade({
@@ -74,19 +158,147 @@ class BitstampClient extends EventEmitter {
     this.emit("trade", trade);
   }
 
-  _sendSubscribe(remote_id) {
-    let channelName = this._channelName(remote_id);
+  _sendSubLevel2Spot(remote_id) {
+    let channelName = remote_id === "btcusd" ? "order_book" : `order_book_${remote_id}`;
     let channel = this._pusher.subscribe(channelName);
-    channel.bind("trade", this._onMessage.bind(this, remote_id));
+    channel.bind("data", this._onLevel2Spot.bind(this, remote_id));
   }
 
-  _sendUnsubscribe(remote_id) {
-    let channelName = this._channelName(remote_id);
+  _sendUnsubLevel2Spot(remote_id) {
+    let channelName = remote_id === "btcusd" ? "order_book" : `order_book_${remote_id}`;
     this._pusher.unsubscribe(channelName);
   }
 
-  _channelName(market) {
-    return market === "btcusd" ? "live_trades" : `live_trades_${market}`;
+  _onLevel2Spot(remote_id, msg) {
+    let market = this._level2SpotSubs.get(remote_id);
+    let { bids, asks, timestamp } = msg;
+    /*
+    {
+      "timestamp": "1528754789",
+      "bids": [
+        ["6778.10", "0.30000000"],
+        ["6778.01", "0.02490239"],
+        ["6778.00", "0.78593442"],
+        ["6777.35", "20.23652315"],
+        ["6776.20", "5.00000000"]
+      ],
+      "asks": [
+        ["6780.00", "28.96553416"],
+        ["6784.22", "0.00116447"],
+        ["6788.21", "0.01000000"],
+        ["6788.73", "1.00000000"],
+        ["6790.00", "1.00000000"]
+      ]
+    }
+    */
+
+    bids = bids.map(([price, size]) => ({ price, size }));
+    asks = asks.map(([price, size]) => ({ price, size }));
+
+    let spot = {
+      exchange: "Bitstamp",
+      base: market.base,
+      quote: market.quote,
+      timestamp,
+      bids,
+      asks,
+    };
+
+    this.emit("l2spot", spot);
+  }
+
+  _sendSubLevel2Updates(remote_id) {
+    let channelName = remote_id === "btcusd" ? "diff_order_book" : `diff_order_book_${remote_id}`;
+    let channel = this._pusher.subscribe(channelName);
+    channel.bind("data", this._onLevel2Update.bind(this, remote_id));
+  }
+
+  _sendUnsubLevel2Updates(remote_id) {
+    let channelName = remote_id === "btcusd" ? "diff_order_book" : `diff_order_book_${remote_id}`;
+    this._pusher.unsubscribe(channelName);
+  }
+
+  _onLevel2Update(remote_id, msg) {
+    let market = this._level2UpdateSubs.get(remote_id);
+    let { bids, asks, timestamp } = msg;
+    /*
+    {
+      "timestamp": "1528755218",
+      "bids": [
+        ["6762.72", "0.00000000"],
+        ["6762.70", "0.00000000"],
+        ["6759.82", "0.07750000"],
+        ["6759.70", "1.47580000"],
+        ["6745.50", "1.95000000"],
+        ["6734.61", "0.46150000"],
+        ["6733.82", "0.00000000"],
+        ["6732.99", "0.00000000"]
+      ],
+      "asks": [
+        ["6778.78", "0.28855655"],
+        ["6778.79", "6.67991600"],
+        ["6778.80", "1.47460000"],
+        ["6778.88", "0.00000000"],
+        ["6778.90", "0.00000000"],
+        ["6779.99", "0.00000000"]
+      ]
+    }
+    */
+
+    bids = bids.map(([price, size]) => ({ price, size }));
+    asks = asks.map(([price, size]) => ({ price, size }));
+
+    let update = {
+      exchange: "Bitstamp",
+      base: market.base,
+      quote: market.quote,
+      timestamp,
+      bids,
+      asks,
+    };
+
+    this.emit("l2update", update);
+  }
+
+  _sendSubLevel3Updates(remote_id) {
+    let channelName = remote_id === "btcusd" ? "live_orders" : `live_orders${remote_id}`;
+    let channel = this._pusher.subscribe(channelName);
+    channel.bind("order_created", this._onLevel3Update.bind(this, remote_id, "created"));
+    channel.bind("order_changed", this._onLevel3Update.bind(this, remote_id, "changed"));
+    channel.bind("order_deleted", this._onLevel3Update.bind(this, remote_id, "deleted"));
+  }
+
+  _sendUnsubLevel3Updates(remote_id) {
+    let channelName = remote_id === "btcusd" ? "live_orders" : `live_orders${remote_id}`;
+    this._pusher.unsubscribe(channelName);
+  }
+
+  _onLevel3Update(remote_id, type, msg) {
+    let market = this._level3UpdateSubs.get(remote_id);
+
+    /*
+    {
+      order_type: 1,
+      price: 6844.1,
+      datetime: '1528757709',
+      amount: 2.66106012,
+      id: 1667122035,
+      microtimestamp: '1528757717001990'
+    }
+    */
+
+    let update = {
+      exchange: "Bitstamp",
+      base: market.base,
+      quote: market.quote,
+      type,
+      time: msg.microtimestamp,
+      order_id: msg.id,
+      price: msg.price,
+      side: msg.order_type === 0 ? "bid" : "ask",
+    };
+
+    this.emit("l3update", update);
   }
 }
 
