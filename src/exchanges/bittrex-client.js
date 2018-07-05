b5c5c4fbe4fdfbbd490d108995811701faa96da4
Brian Mancini <bmancini@gmail.com>
2018-07-05 16:01:17 -0400

    Direct implementation of bittrex api

--- src/exchanges/bittrex-client.js
+++ src/exchanges/bittrex-client.js
@@ -1,85 +1,176 @@
 const { EventEmitter } = require("events");
+const crypto = require("crypto");
 const winston = require("winston");
 const moment = require("moment");
-const bittrex = require("node-bittrex-api");
+const cloudscraper = require("cloudscraper");
+const signalr = require("signalr-client");
 const Trade = require("../trade");
+const Watcher = require("../watcher");
 
 class BittrexClient extends EventEmitter {
   constructor() {
     super();
-    this._subscribed = new Map();
-    this._surrogateId = Date.now();
+    this._retryTimeoutMs = 15000;
+    this._cloudflare; // placeholder for information from cloudflare
+    this._tradeSubs = new Map();
+    this._watcher = new Watcher(this);
+
+    this.hasTrades = true;
+    this.hasLevel2Snapshots = false;
+    this.hasLevel2Updates = false;
+    this.hasLevel3Snapshots = false;
+    this.hasLevel3Updates = false;
   }
 
-  _newSurrogateId() {
-    this._surrogateId++;
-    return this._surrogateId;
+  close(emitEvent = true) {
+    this._watcher.stop();
+    if (this._wss) {
+      try {
+        this._wss.end();
+      } catch (e) {
+        // ignore
+      }
+      this._wss = undefined;
+    }
+    if (emitEvent) this.emit("closed");
+  }
+
+  reconnect(emitEvent = true) {
+    this.close(false);
+    this._connect();
+    if (emitEvent) this.emit("reconnected");
   }
 
   subscribeTrades(market) {
+    this._connect();
     let remote_id = market.id;
-    if (!this._subscribed.has(remote_id)) {
-      winston.info("subscribing to", "Bittrex", remote_id);
-      this._subscribed.set(remote_id, market);
-      this._debounceReconnect();
+
+    if (!this._tradeSubs.has(remote_id)) {
+      winston.info("subscribing to trades", "Bittrex", remote_id);
+      this._tradeSubs.set(remote_id, market);
+
+      if (this._wss) {
+        this._sendSubTrades(remote_id);
+      }
     }
   }
 
   unsubscribeTrades(market) {
     let remote_id = market.id;
-    if (this._subscribed.has(remote_id)) {
+    if (this._tradeSubs.has(remote_id)) {
       winston.info("unsubscribing from", "Bittrex", remote_id);
-      this._subscribed.delete(remote_id);
-      this._debounceReconnect();
+      this._tradeSubs.delete(remote_id);
+
+      if (this._wss) {
+        this._sendUnSubTrades(remote_id);
+      }
     }
   }
 
-  _debounceReconnect() {
-    clearTimeout(this._reconnectTimeout);
-    this._reconnectTimeout = setTimeout(() => {
-      // if connection exists, close to trigger reconnect with new pairs
-      if (this._client) this._client.end();
-      else {
-        bittrex.options({
-          verbose: false,
-          websockets: {
-            onConnect: () => {
-              let pairs = Array.from(this._subscribed.keys());
-              if (pairs.length) {
-                bittrex.websockets.subscribe(pairs, data => {
-                  this._onMessage(data);
-                });
-              }
-            },
-          },
-        });
-        this._connect();
-      }
-    }, 100);
-  }
+  ////////////////////////////////////
 
-  _connect() {
-    bittrex.websockets.client(client => {
-      this._client = client;
-      client.serviceHandlers.connectFailed = err => winston.error("Bittrex", err.message);
-      client.serviceHandlers.onerror = err => winston.error("Bittrex", err.message);
+  _sendSubTrades(remote_id) {
+    this._wss.call("CoreHub", "SubscribeToExchangeDeltas", remote_id).done(err => {
+      if (err) winston.error("subscribe failed", remote_id);
     });
   }
 
-  _onMessage(raw) {
-    if (raw.M === "updateExchangeState") {
-      raw.A.forEach(data => {
-        data.Fills.forEach(fill => {
-          let trade = this._constructTradeFromMessage(fill, data.MarketName);
-          this.emit("trade", trade);
-        });
+  _sendUnSubTrades(remote_id) {
+    this._wss.call("CoreHub", "UnsubscribeToExchangeDeltas", remote_id).done(err => {
+      if (err) winston.error("ussubscribe failed", remote_id);
+    });
+  }
+
+  async _connectCloudflare() {
+    return new Promise((resolve, reject) => {
+      winston.info("cloudflare connection to https://bittrex.com/");
+      cloudscraper.get("https://bittrex.com/", (err, res) => {
+        if (err) return reject(err);
+        else
+          resolve({
+            cookie: res.request.headers["cookie"] || "",
+            user_agent: res.request.headers["User-Agent"] || "",
+          });
       });
+    });
+  }
+
+  async _connect() {
+    // ignore wss creation is we already are connected
+    if (this._wss) return;
+
+    // connect to cloudflare once and cache the promise
+    if (!this._cloudflare) this._cloudflare = this._connectCloudflare();
+
+    // wait for single connection to cloudflare
+    let metadata = await this._cloudflare;
+
+    // doublecheck if wss was already created
+    if (this._wss) return;
+
+    let wss = (this._wss = new signalr.client(
+      "wss://socket.bittrex.com/signalr", // service url
+      ["CoreHub"], // hubs
+      undefined, // disable reconnection
+      true // wait till .start() called
+    ));
+
+    wss.headers["User-Agent"] = metadata.user_agent;
+    wss.headers["cookie"] = metadata.cookie;
+
+    wss.start();
+    wss.serviceHandlers = {
+      connected: this._onConnected.bind(this),
+      disconnected: this._onDisconnected.bind(this),
+      messageReceived: this._onMessage.bind(this),
+      onerror: err => winston.error("error", err).error,
+      connectionlost: err => winston.error("connectionlost", err),
+      connectfailed: err => winston.error("connectfailed", err),
+      reconnecting: () => true, // disables reconnection
+    };
+  }
+
+  _onConnected() {
+    winston.info("connected to wss://socket.bittrex.com/signalr");
+    clearTimeout(this._reconnectHandle);
+    this.emit("connected");
+    this._watcher.start();
+    for (let marketSymbol of this._tradeSubs.keys()) {
+      this._sendSubTrades(marketSymbol);
+    }
+  }
+
+  _onDisconnected() {
+    clearTimeout(this._reconnectHandle);
+    this._watcher.stop();
+    this.emit("disconnected");
+    this._reconnectHandle = setTimeout(() => this.reconnect(false), this._retryTimeoutMs);
+  }
+
+  _onMessage(raw) {
+    // message format
+    // { type: 'utf8', utf8Data: '{"C":"d-5ED873F4-C,0|Ejin,0|Ejio,2|I:,67FC","M":[{"H":"CoreHub","M":"updateExchangeState","A":[{"MarketName":"BTC-ETH","Nounce":26620,"Buys":[{"Type":0,"Rate":0.07117610,"Quantity":7.22300000},{"Type":1,"Rate":0.07117608,"Quantity":0.0},{"Type":0,"Rate":0.07114400,"Quantity":0.08000000},{"Type":0,"Rate":0.07095001,"Quantity":0.46981436},{"Type":1,"Rate":0.05470000,"Quantity":0.0},{"Type":1,"Rate":0.05458200,"Quantity":0.0}],"Sells":[{"Type":2,"Rate":0.07164500,"Quantity":21.55180000},{"Type":1,"Rate":0.07179460,"Quantity":0.0},{"Type":0,"Rate":0.07180300,"Quantity":6.96349769},{"Type":0,"Rate":0.07190173,"Quantity":0.27815742},{"Type":1,"Rate":0.07221246,"Quantity":0.0},{"Type":0,"Rate":0.07223299,"Quantity":58.39672846},{"Type":1,"Rate":0.07676211,"Quantity":0.0}],"Fills":[]}]}]}' }
+
+    if (!raw.utf8Data) return;
+    raw = JSON.parse(raw.utf8Data);
+
+    if (!raw.M) return;
+
+    for (let msg of raw.M) {
+      if (msg.M === "updateExchangeState") {
+        msg.A.forEach(data => {
+          data.Fills.forEach(fill => {
+            let trade = this._constructTradeFromMessage(fill, data.MarketName);
+            this.emit("trade", trade);
+          });
+        });
+      }
     }
   }
 
   _constructTradeFromMessage(msg, marketName) {
-    let market = this._subscribed.get(marketName);
-    let trade_id = this._newSurrogateId();
+    let market = this._tradeSubs.get(marketName);
+    let tradeId = this._getTradeId(msg);
     let unix = moment.utc(msg.TimeStamp).unix();
     let price = parseFloat(msg.Rate);
     let amount = msg.OrderType === "BUY" ? parseFloat(msg.Quantity) : -parseFloat(msg.Quantity);
@@ -88,11 +179,23 @@ class BittrexClient extends EventEmitter {
       exchange: "Bittrex",
       base: market.base,
       quote: market.quote,
-      tradeId: trade_id,
+      tradeId,
       unix,
       price,
       amount,
     });
+  }
+
+  _getTradeId(msg) {
+    let ms = moment.utc(msg.TimeStamp).valueOf();
+    let buysell = msg.OrderType === "BUY" ? 1 : 0;
+    let price = msg.Rate.toFixed(8);
+    let amount = msg.Quantity.toFixed(8);
+    let preimage = `${ms}:${buysell}:${price}:${amount}`;
+    let hasher = crypto.createHash("md5");
+    hasher.update(preimage);
+    let tradeId = hasher.digest().toString("hex");
+    return tradeId;
   }
 }
 
---
+++ src/exchanges/bittrex-client.spec.js
@@ -0,0 +1,129 @@
+const Bittrex = require("./bittrex-client");
+jest.mock("winston", () => ({ info: jest.fn() }));
+
+let client;
+let market = {
+  id: "USDT-BTC",
+  base: "BTC",
+  quote: "USDT",
+};
+
+beforeAll(() => {
+  client = new Bittrex();
+});
+
+test("it should support trades", () => {
+  expect(client.hasTrades).toBeTruthy();
+});
+
+test("it should not support level2 snapshots", () => {
+  expect(client.hasLevel2Snapshots).toBeFalsy();
+});
+
+test("it should not support level2 updates", () => {
+  expect(client.hasLevel2Updates).toBeFalsy();
+});
+
+test("it should not support level3 snapshots", () => {
+  expect(client.hasLevel3Snapshots).toBeFalsy();
+});
+
+test("it should not support level3 updates", () => {
+  expect(client.hasLevel3Updates).toBeFalsy();
+});
+
+test(
+  "should subscribe and emit trade events",
+  done => {
+    client.subscribeTrades(market);
+    client.on("trade", trade => {
+      expect(trade.fullId).toMatch("Bittrex:BTC/USDT");
+      expect(trade.exchange).toMatch("Bittrex");
+      expect(trade.base).toMatch("BTC");
+      expect(trade.quote).toMatch("USDT");
+      expect(trade.tradeId).toMatch(/^[0-9a-f]{32,32}$/);
+      expect(trade.unix).toBeGreaterThan(1522540800);
+      expect(trade.price).toBeGreaterThan(0);
+      expect(trade.amount).toBeDefined();
+      expect(trade.buyOrderId).toBeUndefined();
+      expect(trade.sellOrderId).toBeUndefined();
+      done();
+    });
+  },
+  30000
+);
+
+// test("should subscribe and emit level2 snapshots", done => {
+//   client.subscribeLevel2Snapshots(market);
+//   client.on("l2snapshot", snapshot => {
+//     expect(snapshot.fullId).toMatch("Bitstamp:BTC/USD");
+//     expect(snapshot.exchange).toMatch("Bitstamp");
+//     expect(snapshot.base).toMatch("BTC");
+//     expect(snapshot.quote).toMatch("USD");
+//     expect(snapshot.sequenceId).toBeUndefined();
+//     expect(snapshot.timestampMs).toBeGreaterThan(0);
+//     expect(parseFloat(snapshot.asks[0].price)).toBeGreaterThanOrEqual(0);
+//     expect(parseFloat(snapshot.asks[0].size)).toBeGreaterThanOrEqual(0);
+//     expect(snapshot.asks[0].count).toBeUndefined();
+//     expect(parseFloat(snapshot.bids[0].price)).toBeGreaterThanOrEqual(0);
+//     expect(parseFloat(snapshot.bids[0].size)).toBeGreaterThanOrEqual(0);
+//     expect(snapshot.bids[0].count).toBeUndefined();
+//     done();
+//   });
+// });
+
+// test("should subscribe and emit level2 updates", done => {
+//   client.subscribeLevel2Updates(market);
+//   client.on("l2update", update => {
+//     expect(update.fullId).toMatch("Bitstamp:BTC/USD");
+//     expect(update.exchange).toMatch("Bitstamp");
+//     expect(update.base).toMatch("BTC");
+//     expect(update.quote).toMatch("USD");
+//     expect(update.sequenceId).toBeUndefined();
+//     expect(update.timestampMs).toBeGreaterThan(0);
+//     let point = update.asks[0] || update.bids[0];
+//     expect(parseFloat(point.price)).toBeGreaterThanOrEqual(0);
+//     expect(parseFloat(point.size)).toBeGreaterThanOrEqual(0);
+//     expect(point.count).toBeUndefined();
+//     done();
+//   });
+// });
+
+// test("should subscribe and emit level3 updates", done => {
+//   client.subscribeLevel3Updates(market);
+//   client.on("l3update", update => {
+//     expect(update.fullId).toMatch("Bitstamp:BTC/USD");
+//     expect(update.exchange).toMatch("Bitstamp");
+//     expect(update.base).toMatch("BTC");
+//     expect(update.quote).toMatch("USD");
+//     expect(update.sequenceId).toBeUndefined();
+//     expect(update.timestampMs).toBeGreaterThan(0);
+//     let point = update.asks[0] || update.bids[0];
+//     expect(point.orderId).toBeGreaterThan(0);
+//     expect(parseFloat(point.price)).toBeGreaterThanOrEqual(0);
+//     expect(parseFloat(point.size)).toBeGreaterThanOrEqual(0);
+//     expect(point.meta.type).toMatch(/created|updated|deleted/);
+//     done();
+//   });
+// });
+
+test("should unsubscribe from trade events", () => {
+  client.unsubscribeTrades(market);
+});
+
+// test("should unsubscribe from level2 snapshot", () => {
+//   client.unsubscribeLevel2Snapshots(market);
+// });
+
+// test("should unsubscribe from level2 updates", () => {
+//   client.unsubscribeLevel2Updates(market);
+// });
+
+// test("should unsubscribe from level3 updates", () => {
+//   client.unsubscribeLevel3Updates(market);
+// });
+
+test("should close connections", done => {
+  client.on("closed", done);
+  client.close();
+});
