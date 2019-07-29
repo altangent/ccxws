e3708961460589c448a150904adcba0cfe2bae7d
Brian Mancini <bmancini@gmail.com>
2019-07-29 11:53:20 -0400

    creating test commands in package.json

--- __tests__/bibox-client.spec.js
+++ __tests__/bibox-client.spec.js
@@ -51,32 +51,3 @@ testClient({
     hasCount: false,
   },
 });
-
-//   test("should subscribe and emit level2 snapshots", done => {
-//     client.subscribeLevel2Snapshots(market1);
-//     client.on("l2snapshot", (snapshot, market) => {
-//       expect(market).toBeDefined();
-//       expect(market.id).toMatch(/BTC_USDT|ETH_BTC/);
-//       expect(snapshot.fullId).toMatch(/Bibox:BTC\/USDT/);
-//       expect(snapshot.exchange).toMatch("Bibox");
-//       expect(snapshot.base).toMatch(market1.base);
-//       expect(snapshot.quote).toMatch(market1.quote);
-//       expect(snapshot.timestampMs).toBeGreaterThan(1553712743791);
-//       expect(snapshot.sequenceId).toBeUndefined();
-//       if (snapshot.asks.length) {
-//         expect(parseFloat(snapshot.asks[0].price)).toBeGreaterThanOrEqual(0);
-//         expect(parseFloat(snapshot.asks[0].size)).toBeGreaterThanOrEqual(0);
-//       }
-//       if (snapshot.bids.length) {
-//         expect(parseFloat(snapshot.bids[0].price)).toBeGreaterThanOrEqual(0);
-//         expect(parseFloat(snapshot.bids[0].size)).toBeGreaterThanOrEqual(0);
-//       }
-//       done();
-//     });
-//   }, 30000);
-
-//   test("should close connections", done => {
-//     client.on("closed", done);
-//     client.close();
-//   });
-// });
