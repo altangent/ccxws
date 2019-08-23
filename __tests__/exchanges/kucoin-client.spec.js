f7b5ff30f78da61169efd88a82c0b464b8bbb9a3
Benoist Claassen <benoist.claassen@gmail.com>
2019-08-23 11:22:08 +0200

    Added KuCoin v2

---
+++ __tests__/exchanges/kucoin-client.spec.js
@@ -0,0 +1,61 @@
+const { testClient } = require("../test-runner");
+const KucoinClient = require("../../src/exchanges/kucoin-client");
+
+testClient({
+  clientFactory: () => new KucoinClient(),
+  clientName: "KucoinClient",
+  exchangeName: "KuCoin",
+  markets: [
+    {
+      id: "BTC-USDT",
+      base: "BTC",
+      quote: "USDT",
+    },
+  ],
+
+  testConnectEvents: false,
+  testDisconnectEvents: false,
+  testReconnectionEvents: false,
+  testCloseEvents: false,
+
+  hasTickers: true,
+  hasTrades: true,
+  hasLevel2Snapshots: false,
+  hasLevel2Updates: true,
+  hasLevel3Snapshots: false,
+  hasLevel3Updates: false,
+
+  ticker: {
+    hasTimestamp: true,
+    hasLast: true,
+    hasOpen: true,
+    hasHigh: true,
+    hasLow: true,
+    hasVolume: true,
+    hasQuoteVolume: false,
+    hasChange: true,
+    hasChangePercent: true,
+    hasAsk: true,
+    hasBid: true,
+    hasAskVolume: false,
+    hasBidVolume: false,
+  },
+
+  trade: {
+    hasTradeId: true,
+    tradeIdPattern: /\w{24,}/,
+  },
+
+  l2snapshot: {
+    hasTimestampMs: false,
+    hasSequenceId: false,
+    hasCount: false,
+  },
+
+  l2update: {
+    hasSnapshot: true,
+    hasTimestampMs: true,
+    hasSequenceId: false,
+    hasCount: false,
+  },
+});
--- src/https.js
+++ src/https.js
@@ -3,6 +3,7 @@ const url = require("url");
 
 module.exports = {
   get,
+  post,
 };
 
 async function get(uri) {
@@ -22,6 +23,41 @@ async function get(uri) {
       });
     });
     req.on("error", reject);
+    req.end();
+  });
+}
+
+async function post(uri, postData = "") {
+  return new Promise((resolve, reject) => {
+    let { hostname, port, pathname } = url.parse(uri);
+
+    let req = https.request(
+      {
+        host: hostname,
+        port,
+        path: pathname,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": postData.length
+        }
+      },
+      res => {
+        let results = [];
+        res.on("error", reject);
+        res.on("data", data => results.push(data));
+        res.on("end", () => {
+          results = Buffer.concat(results);
+          if (res.statusCode !== 200) {
+            return reject(results.toString());
+          } else {
+            return resolve(JSON.parse(results));
+          }
+        });
+      }
+    );
+    req.on("error", reject);
+    req.write(postData);
     req.end();
   });
 }
