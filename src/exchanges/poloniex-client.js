b0deedaf7705085366d645197ae45545023b66f2
Brian Mancini <bmancini@gmail.com>
2019-10-24 08:34:58 -0400

    poloniex: dynamic market symbol map
    Poloniex tickers use a numeric identifier. Prior to this commit,
    identifiers were maintained in a static list that would need to be
    periodically updated.
    
    This commit uses the REST ticker API to obtain the identifiers and
    dynamically build the lookup when the class is instantiated. An
    optional flag is added to the constructor (following the same pattern
    used by Kraken).

--- src/exchanges/poloniex-client.js
+++ src/exchanges/poloniex-client.js
@@ -4,141 +4,10 @@ const Trade = require("../trade");
 const Level2Point = require("../level2-point");
 const Level2Update = require("../level2-update");
 const Level2Snapshot = require("../level2-snapshot");
-
-const TICKERS_ID = 1002;
-const MARKET_IDS = {
-  177: "BTC_ARDR",
-  253: "BTC_ATOM",
-  210: "BTC_BAT",
-  189: "BTC_BCH",
-  236: "BTC_BCHABC",
-  238: "BTC_BCHSV",
-  7: "BTC_BCN",
-  232: "BTC_BNT",
-  14: "BTC_BTS",
-  15: "BTC_BURST",
-  20: "BTC_CLAM",
-  194: "BTC_CVC",
-  24: "BTC_DASH",
-  162: "BTC_DCR",
-  25: "BTC_DGB",
-  27: "BTC_DOGE",
-  201: "BTC_EOS",
-  171: "BTC_ETC",
-  148: "BTC_ETH",
-  155: "BTC_FCT",
-  246: "BTC_FOAM",
-  38: "BTC_GAME",
-  198: "BTC_GAS",
-  185: "BTC_GNT",
-  251: "BTC_GRIN",
-  43: "BTC_HUC",
-  207: "BTC_KNC",
-  167: "BTC_LBC",
-  213: "BTC_LOOM",
-  250: "BTC_LPT",
-  163: "BTC_LSK",
-  50: "BTC_LTC",
-  51: "BTC_MAID",
-  229: "BTC_MANA",
-  61: "BTC_NAV",
-  64: "BTC_NMC",
-  248: "BTC_NMR",
-  69: "BTC_NXT",
-  196: "BTC_OMG",
-  58: "BTC_OMNI",
-  184: "BTC_PASC",
-  249: "BTC_POLY",
-  75: "BTC_PPC",
-  221: "BTC_QTUM",
-  174: "BTC_REP",
-  170: "BTC_SBD",
-  150: "BTC_SC",
-  204: "BTC_SNT",
-  168: "BTC_STEEM",
-  200: "BTC_STORJ",
-  89: "BTC_STR",
-  182: "BTC_STRAT",
-  92: "BTC_SYS",
-  97: "BTC_VIA",
-  100: "BTC_VTC",
-  108: "BTC_XCP",
-  112: "BTC_XEM",
-  114: "BTC_XMR",
-  116: "BTC_XPM",
-  117: "BTC_XRP",
-  178: "BTC_ZEC",
-  192: "BTC_ZRX",
-  211: "ETH_BAT",
-  190: "ETH_BCH",
-  233: "ETH_BNT",
-  195: "ETH_CVC",
-  202: "ETH_EOS",
-  172: "ETH_ETC",
-  199: "ETH_GAS",
-  186: "ETH_GNT",
-  208: "ETH_KNC",
-  214: "ETH_LOOM",
-  166: "ETH_LSK",
-  230: "ETH_MANA",
-  197: "ETH_OMG",
-  222: "ETH_QTUM",
-  176: "ETH_REP",
-  205: "ETH_SNT",
-  169: "ETH_STEEM",
-  179: "ETH_ZEC",
-  193: "ETH_ZRX",
-  254: "USDC_ATOM",
-  235: "USDC_BCH",
-  237: "USDC_BCHABC",
-  239: "USDC_BCHSV",
-  224: "USDC_BTC",
-  243: "USDC_DOGE",
-  225: "USDC_ETH",
-  247: "USDC_FOAM",
-  252: "USDC_GRIN",
-  244: "USDC_LTC",
-  242: "USDC_STR",
-  226: "USDC_USDT",
-  241: "USDC_XMR",
-  240: "USDC_XRP",
-  245: "USDC_ZEC",
-  255: "USDT_ATOM",
-  212: "USDT_BAT",
-  191: "USDT_BCH",
-  234: "USDT_BNT",
-  121: "USDT_BTC",
-  122: "USDT_DASH",
-  216: "USDT_DOGE",
-  203: "USDT_EOS",
-  173: "USDT_ETC",
-  149: "USDT_ETH",
-  217: "USDT_GNT",
-  209: "USDT_KNC",
-  215: "USDT_LOOM",
-  218: "USDT_LSK",
-  123: "USDT_LTC",
-  231: "USDT_MANA",
-  124: "USDT_NXT",
-  223: "USDT_QTUM",
-  175: "USDT_REP",
-  219: "USDT_SC",
-  206: "USDT_SNT",
-  125: "USDT_STR",
-  126: "USDT_XMR",
-  127: "USDT_XRP",
-  180: "USDT_ZEC",
-  220: "USDT_ZRX",
-  129: "XMR_BCN",
-  132: "XMR_DASH",
-  137: "XMR_LTC",
-  138: "XMR_MAID",
-  140: "XMR_NXT",
-  181: "XMR_ZEC",
-};
+const https = require("../https");
 
 class PoloniexClient extends BasicClient {
-  constructor() {
+  constructor({ autoloadSymbolMaps = true } = {}) {
     super("wss://api2.poloniex.com/", "Poloniex");
     this._idMap = new Map();
     this.hasTickers = true;
@@ -146,6 +15,30 @@ class PoloniexClient extends BasicClient {
     this.hasLevel2Updates = true;
     this._subbedToTickers = false;
     this.on("connected", this._resetSubCount.bind(this));
+
+    this.TICKERS_ID = 1002;
+    this.MARKET_IDS = new Map();
+
+    if (autoloadSymbolMaps) {
+      this.loadSymbolMaps().catch(err => this.emit("error", err));
+    }
+  }
+
+  /**
+    Poloniex uses numeric identifiers for its markets.
+    A static map of these markets can be obtained from:
+    https://docs.poloniex.com/#currency-pair-ids
+
+    We can use the ticker REST API as a mechanism to obtain
+    the identifiers and create an index of id to symbol.
+   */
+  async loadSymbolMaps() {
+    let uri = "https://poloniex.com/public?command=returnTicker";
+    let result = await https.get(uri);
+    for (let symbol in result) {
+      let id = result[symbol].id;
+      this.MARKET_IDS.set(id, symbol);
+    }
   }
 
   _resetSubCount() {
@@ -158,7 +51,7 @@ class PoloniexClient extends BasicClient {
     this._wss.send(
       JSON.stringify({
         command: "subscribe",
-        channel: TICKERS_ID,
+        channel: this.TICKERS_ID,
       })
     );
   }
@@ -169,7 +62,7 @@ class PoloniexClient extends BasicClient {
     this._wss.send(
       JSON.stringify({
         command: "unsubscribe",
-        channel: TICKERS_ID,
+        channel: this.TICKERS_ID,
       })
     );
   }
@@ -228,8 +121,8 @@ class PoloniexClient extends BasicClient {
     let updates = msg[2];
 
     // tickers
-    if (id === 1002 && updates) {
-      let remoteId = MARKET_IDS[updates[0]];
+    if (id === this.TICKERS_ID && updates) {
+      let remoteId = this.MARKET_IDS.get(updates[0]);
       let market = this._tickerSubs.get(remoteId);
       if (!market) return;
 
