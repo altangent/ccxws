be00341f5328b7fd890e19166a5fa307de0e3cb8
Brian Mancini <bmancini@gmail.com>
2020-08-03 13:31:09 -0400

    kraken: adding prototype order book
    This adds a new prototype order book implementation that conforms to the
    specifications outlined in the Kraken API documentation.

---
+++ src/orderbooks/KrakenOrderBookPoint.js
@@ -0,0 +1,9 @@
+class KrakenOrderBookPoint {
+  constructor(price, size, timestamp) {
+    this.price = price;
+    this.size = size;
+    this.timestamp = timestamp;
+  }
+}
+
+module.exports = KrakenOrderBookPoint;
