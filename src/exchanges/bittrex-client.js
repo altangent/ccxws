775906b4b33d759683f7ea5b4d449d1f3d1e86db
Brian Mancini <bmancini@gmail.com>
2020-08-17 17:26:43 -0400

    bittrex: initial conversion to v3

--- src/exchanges/bittrex-client.js
+++ src/exchanges/bittrex-client.js
@@ -1,22 +1,29 @@
+const zlib = require("../zlib");
 const { EventEmitter } = require("events");
-const crypto = require("crypto");
 const moment = require("moment");
-const cloudscraper = require("cloudscraper");
-const signalr = require("signalr-client");
+const turdr = require("signalr-client");
 const Watcher = require("../watcher");
 const Ticker = require("../ticker");
 const Trade = require("../trade");
+const Candle = require("../candle");
 const Level2Snapshot = require("../level2-snapshot");
 const Level2Update = require("../level2-update");
 const Level2Point = require("../level2-point");
+const { CandlePeriod } = require("../enums");
 
+/**
+ * Implements the v3 API:
+ * https://bittrex.github.io/api/v3#topic-Synchronizing
+ * https://bittrex.github.io/guides/v3/upgrade
+ */
 class BittrexClient extends EventEmitter {
   constructor() {
     super();
+    this._name = "Bittrex";
     this._retryTimeoutMs = 15000;
-    this._cloudflare; // placeholder for information from cloudflare
     this._tickerSubs = new Map();
     this._tradeSubs = new Map();
+    this._candleSubs = new Map();
     this._level2UpdateSubs = new Map();
     this._watcher = new Watcher(this);
     this._isConnected = false;
@@ -30,6 +37,13 @@ class BittrexClient extends EventEmitter {
     this.hasLevel2Updates = true;
     this.hasLevel3Snapshots = false;
     this.hasLevel3Updates = false;
+    this.candlePeriod = CandlePeriod._1m;
+    this.orderBookDepth = 500;
+
+    this._processTickers = this._processTickers.bind(this);
+    this._processTrades = this._processTrades.bind(this);
+    this._processCandles = this._processCandles.bind(this);
+    this._processLevel2Update = this._processLevel2Update.bind(this);
   }
 
   close(emitEvent = true) {
@@ -77,19 +91,27 @@ class BittrexClient extends EventEmitter {
   }
 
   subscribeTrades(market) {
-    this._subscribe(market, this._tradeSubs);
-  }
-
-  subscribeLevel2Updates(market) {
-    this._subscribe(market, this._level2UpdateSubs);
+    this._subscribe(market, this._tradeSubs, this._sendSubTrades.bind(this));
   }
 
   unsubscribeTrades(market) {
-    this._unsubscribe(market, this._tradeSubs);
+    this._unsubscribe(market, this._tradeSubs, this._sendUnsubTrades.bind(this));
+  }
+
+  subscribeCandles(market) {
+    this._subscribe(market, this._candleSubs, this._sendSubCandles.bind(this));
+  }
+
+  unsubscribeCandles(market) {
+    this._unsubscribe(market, this._candleSubs, this._sendUnsubCandles.bind(this));
+  }
+
+  subscribeLevel2Updates(market) {
+    this._subscribe(market, this._level2UpdateSubs, this._sendSubLevel2Updates.bind(this));
   }
 
   unsubscribeLevel2Updates(market) {
-    this._unsubscribe(market, this._level2UpdateSubs);
+    this._unsubscribe(market, this._level2UpdateSubs, this._sendUnsubLevel2Updates.bind(this));
   }
 
   ////////////////////////////////////
@@ -99,7 +121,7 @@ class BittrexClient extends EventEmitter {
     this._subCount = {};
   }
 
-  _subscribe(market, map) {
+  _subscribe(market, map, subFn) {
     this._connect();
     let remote_id = market.id;
 
@@ -107,7 +129,7 @@ class BittrexClient extends EventEmitter {
       map.set(remote_id, market);
 
       if (this._isConnected) {
-        this._sendSub(remote_id);
+        subFn(remote_id, market);
       }
     }
   }
@@ -123,30 +145,6 @@ class BittrexClient extends EventEmitter {
     }
   }
 
-  _sendSub(remote_id) {
-    // increment market counter
-    this._subCount[remote_id] = (this._subCount[remote_id] || 0) + 1;
-
-    // if we have more than one sub, ignore the request as we're already subbed
-    if (this._subCount[remote_id] > 1) return;
-
-    // initiate snapshot request
-    if (this._level2UpdateSubs.has(remote_id)) {
-      this._wss.call("CoreHub", "QueryExchangeState", remote_id).done((err, result) => {
-        if (err) return this.emit("error", err);
-        if (!result) return;
-        let market = this._level2UpdateSubs.get(remote_id);
-        let snapshot = this._constructLevel2Snapshot(result, market);
-        this.emit("l2snapshot", snapshot, market);
-      });
-    }
-
-    // initiate the subscription
-    this._wss.call("CoreHub", "SubscribeToExchangeDeltas", remote_id).done(err => {
-      if (err) return this.emit("error", err);
-    });
-  }
-
   _sendUnsub(remote_id) {
     // decrement market count
     this._subCount[remote_id] -= 1;
@@ -154,7 +152,7 @@ class BittrexClient extends EventEmitter {
 
   _sendSubTickers() {
     if (this._tickerConnected) return;
-    this._wss.call("CoreHub", "SubscribeToSummaryDeltas").done(err => {
+    this._wss.call("c3", "Subscribe", ["market_summaries"]).done(err => {
       if (err) return this.emit("error", err);
       else this._tickerConnected = true;
     });
@@ -164,50 +162,58 @@ class BittrexClient extends EventEmitter {
     // no-op
   }
 
-  async _connectCloudflare() {
-    return new Promise((resolve, reject) => {
-      cloudscraper.get("https://bittrex.com/", (err, res) => {
-        if (err) return reject(err);
-        else
-          resolve({
-            cookie: res.request.headers["cookie"] || "",
-            user_agent: res.request.headers["User-Agent"] || "",
-          });
-      });
+  _sendSubTrades(remote_id) {
+    this._wss.call("c3", "Subscribe", [`trade_${remote_id}`]).done(err => {
+      if (err) return this.emit("error", err);
     });
+  }
+
+  _sendUnsubTrades(remote_id) {
+    // no-op
+  }
+
+  _sendSubCandles(remote_id) {
+    this._wss
+      .call("c3", "Subscribe", [`candle_${remote_id}_${candlePeriod(this.candlePeriod)}`])
+      .done(err => {
+        if (err) return this.emit("error", err);
+      });
+  }
+
+  _sendUnsubCandles(remote_id) {
+    // no-op
+  }
+
+  _sendSubLevel2Updates(remote_id) {
+    this._wss.call("c3", "Subscribe", [`orderbook_${remote_id}_${this.orderBookDepth}`]);
+  }
+
+  _sendUnsubLevel2Updates(remote_id) {
+    // no-op
   }
 
   async _connect() {
     // ignore wss creation is we already are connected
     if (this._wss) return;
 
-    // connect to cloudflare once and cache the promise
-    if (!this._cloudflare) this._cloudflare = this._connectCloudflare();
-
-    // wait for single connection to cloudflare
-    let metadata = await this._cloudflare;
-
-    // doublecheck if wss was already created
-    if (this._wss) return;
-
-    let wss = (this._wss = new signalr.client(
-      "wss://socket.bittrex.com/signalr", // service url
-      ["CoreHub"], // hubs
+    let wss = (this._wss = new turdr.client(
+      "wss://socket-v3.bittrex.com/signalr", // service url
+      ["c3"], // hubs
       undefined, // disable reconnection
       true // wait till .start() called
     ));
 
-    wss.headers["User-Agent"] = metadata.user_agent;
-    wss.headers["cookie"] = metadata.cookie;
     wss.serviceHandlers = {
+      bindingError: err => this.emit("error", err),
+      connectFailed: () => this.emit("closed"),
       connected: this._onConnected.bind(this),
+      connectionLost: () => this.emit("closed"),
       disconnected: this._onDisconnected.bind(this),
-      messageReceived: this._onMessage.bind(this),
       onerror: err => this.emit("error", err),
-      connectionlost: () => this.emit("closed"),
-      connectfailed: () => this.emit("closed"),
+      messageReceived: this._onMessage.bind(this),
       reconnecting: () => true, // disables reconnection
     };
+
     wss.start();
   }
 
@@ -217,14 +223,17 @@ class BittrexClient extends EventEmitter {
     this._tickerConnected = false;
     this._isConnected = true;
     this.emit("connected");
-    for (let marketSymbol of this._tickerSubs.keys()) {
-      this._sendSubTickers(marketSymbol);
+    for (let [marketSymbol, market] of this._tickerSubs) {
+      this._sendSubTickers(marketSymbol, market);
     }
-    for (let marketSymbol of this._tradeSubs.keys()) {
-      this._sendSub(marketSymbol);
+    for (let [marketSymbol, market] of this._tradeSubs) {
+      this._sendSubTrades(marketSymbol, market);
     }
-    for (let marketSymbol of this._level2UpdateSubs.keys()) {
-      this._sendSub(marketSymbol);
+    for (let [marketSymbol, market] of this._candleSubs) {
+      this._sendSubCandles(marketSymbol, market);
+    }
+    for (let [marketSymbol, market] of this._level2UpdateSubs) {
+      this._sendSubLevel2Updates(marketSymbol, market);
     }
     this._watcher.start();
   }
@@ -241,38 +250,34 @@ class BittrexClient extends EventEmitter {
 
   _onMessage(raw) {
     try {
-      // message format
-      // { type: 'utf8', utf8Data: '{"C":"d-5ED873F4-C,0|Ejin,0|Ejio,2|I:,67FC","M":[{"H":"CoreHub","M":"updateExchangeState","A":[{"MarketName":"BTC-ETH","Nounce":26620,"Buys":[{"Type":0,"Rate":0.07117610,"Quantity":7.22300000},{"Type":1,"Rate":0.07117608,"Quantity":0.0},{"Type":0,"Rate":0.07114400,"Quantity":0.08000000},{"Type":0,"Rate":0.07095001,"Quantity":0.46981436},{"Type":1,"Rate":0.05470000,"Quantity":0.0},{"Type":1,"Rate":0.05458200,"Quantity":0.0}],"Sells":[{"Type":2,"Rate":0.07164500,"Quantity":21.55180000},{"Type":1,"Rate":0.07179460,"Quantity":0.0},{"Type":0,"Rate":0.07180300,"Quantity":6.96349769},{"Type":0,"Rate":0.07190173,"Quantity":0.27815742},{"Type":1,"Rate":0.07221246,"Quantity":0.0},{"Type":0,"Rate":0.07223299,"Quantity":58.39672846},{"Type":1,"Rate":0.07676211,"Quantity":0.0}],"Fills":[]}]}]}' }
-
       if (!raw.utf8Data) return;
+
       raw = JSON.parse(raw.utf8Data);
 
       if (!raw.M) return;
 
       for (let msg of raw.M) {
-        if (msg.M === "updateExchangeState") {
-          for (let data of msg.A) {
-            if (this._tradeSubs.has(data.MarketName)) {
-              let market = this._tradeSubs.get(data.MarketName);
-              for (let fill of data.Fills) {
-                let trade = this._constructTradeFromMessage(fill, market);
-                this.emit("trade", trade, market);
-              }
-            }
-            if (this._level2UpdateSubs.has(data.MarketName)) {
-              let market = this._level2UpdateSubs.get(data.MarketName);
-              let l2update = this._constructLevel2Update(data, market);
-              this.emit("l2update", l2update, market);
-            }
+        if (msg.M === "marketSummaries") {
+          for (let a of msg.A) {
+            zlib.inflateRaw(Buffer.from(a, "base64"), this._processTickers);
           }
         }
-        if (msg.M === "updateSummaryState") {
-          for (let raw of msg.A[0].Deltas) {
-            if (this._tickerSubs.has(raw.MarketName)) {
-              let market = this._tickerSubs.get(raw.MarketName);
-              let ticker = this._constructTicker(raw, market);
-              this.emit("ticker", ticker, market);
-            }
+
+        if (msg.M === "trade") {
+          for (let a of msg.A) {
+            zlib.inflateRaw(Buffer.from(a, "base64"), this._processTrades);
+          }
+        }
+
+        if (msg.M === "candle") {
+          for (let a of msg.A) {
+            zlib.inflateRaw(Buffer.from(a, "base64"), this._processCandles);
+          }
+        }
+
+        if (msg.M === "orderBook") {
+          for (let a of msg.A) {
+            zlib.inflateRaw(Buffer.from(a, "base64"), this._processLevel2Update);
           }
         }
       }
@@ -281,36 +286,111 @@ class BittrexClient extends EventEmitter {
     }
   }
 
+  /**
+   {
+      "sequence": 3584000,
+      "deltas": [
+        {
+          symbol: 'BTC-USDT',
+          high: '12448.02615735',
+          low: '11773.32163568',
+          volume: '640.86060471',
+          quoteVolume: '7714634.67704918',
+          percentChange: '3.98',
+          updatedAt: '2020-08-17T20:16:27.617Z'
+        }
+      ]
+    }
+   */
+  _processTickers(err, raw) {
+    if (err) {
+      this.emit("error", err);
+      return;
+    }
+
+    let msg;
+    try {
+      msg = JSON.parse(raw);
+    } catch (ex) {
+      this.emit("error", ex);
+      return;
+    }
+
+    for (let datum of msg.deltas) {
+      let market = this._tickerSubs.get(datum.symbol);
+      if (!market) continue;
+
+      let ticker = this._constructTicker(datum, market);
+      this.emit("ticker", ticker, market);
+    }
+  }
+
   _constructTicker(msg, market) {
-    let { High, Low, Last, PrevDay, BaseVolume, Volume, TimeStamp, Bid, Ask } = msg;
-    let change = Last - PrevDay;
-    let percentChange = ((Last - PrevDay) / PrevDay) * 100;
+    let { high, low, volume, quoteVolume, percentChange, updatedAt } = msg;
     return new Ticker({
-      exchange: "Bittrex",
+      exchange: this._name,
       base: market.base,
       quote: market.quote,
-      timestamp: moment.utc(TimeStamp).valueOf(),
-      last: Last.toFixed(8),
-      open: PrevDay.toFixed(8),
-      high: High.toFixed(8),
-      low: Low.toFixed(8),
-      volume: Volume.toFixed(8),
-      quoteVolume: BaseVolume.toFixed(8),
-      change: change.toFixed(8),
-      changePercent: percentChange.toFixed(8),
-      bid: Bid.toFixed(8),
-      ask: Ask.toFixed(8),
+      timestamp: moment.utc(updatedAt).valueOf(),
+      last: undefined,
+      open: undefined,
+      high: high,
+      low: low,
+      volume: volume,
+      quoteVolume: quoteVolume,
+      change: undefined,
+      changePercent: percentChange,
+      bid: undefined,
+      ask: undefined,
     });
   }
 
-  _constructTradeFromMessage(msg, market) {
-    let tradeId = this._getTradeId(msg);
-    let unix = moment.utc(msg.TimeStamp).valueOf();
-    let price = msg.Rate.toFixed(8);
-    let amount = msg.Quantity.toFixed(8);
-    let side = msg.OrderType === "BUY" ? "buy" : "sell";
+  /**
+   {
+      deltas: [
+        {
+          id: 'edacd990-7c5f-4c75-8a66-ce0a71093b3c',
+          executedAt: '2020-08-17T20:36:39.96Z',
+          quantity: '0.00714818',
+          rate: '12301.34800000',
+          takerSide: 'BUY'
+        }
+      ],
+      sequence: 18344,
+      marketSymbol: 'BTC-USDT'
+    }
+   */
+  _processTrades(err, raw) {
+    if (err) {
+      this.emit("error", err);
+      return;
+    }
+
+    let msg;
+    try {
+      msg = JSON.parse(raw);
+    } catch (ex) {
+      this.emit("error", ex);
+      return;
+    }
+
+    let market = this._tradeSubs.get(msg.marketSymbol);
+    if (!market) return;
+
+    for (let datum of msg.deltas) {
+      let trade = this._constructTrade(datum, market);
+      this.emit("trade", trade, market);
+    }
+  }
+
+  _constructTrade(msg, market) {
+    let tradeId = msg.id;
+    let unix = moment.utc(msg.executedAt).valueOf();
+    let price = msg.rate;
+    let amount = msg.quantity;
+    let side = msg.takerSide === "BUY" ? "buy" : "sell";
     return new Trade({
-      exchange: "Bittrex",
+      exchange: this._name,
       base: market.base,
       quote: market.quote,
       tradeId,
@@ -321,28 +401,94 @@ class BittrexClient extends EventEmitter {
     });
   }
 
-  // prettier-ignore
-  _constructLevel2Snapshot(msg, market) {
-    let sequenceId = msg.Nonce;
-    let bids = msg.Buys.map(p => new Level2Point(p.Rate.toFixed(8), p.Quantity.toFixed(8), undefined, { type: p.Type }));
-    let asks = msg.Sells.map(p => new Level2Point(p.Rate.toFixed(8), p.Quantity.toFixed(8), undefined, { type: p.Type }));
-    return new Level2Snapshot({
-      exchange: "Bittrex",
-      base: market.base,
-      quote: market.quote,
-      sequenceId,
-      asks,
-      bids,
-    });
+  /**
+   {
+      sequence: 10808,
+      marketSymbol: 'BTC-USDT',
+      interval: 'MINUTE_1',
+      delta: {
+        startsAt: '2020-08-17T20:47:00Z',
+        open: '12311.59599999',
+        high: '12311.59599999',
+        low: '12301.57150000',
+        close: '12301.57150000',
+        volume: '1.65120614',
+        quoteVolume: '20319.96359337'
+      }
+    }
+   */
+  _processCandles(err, raw) {
+    if (err) {
+      this.emit("error", err);
+      return;
+    }
+
+    let msg;
+    try {
+      msg = JSON.parse(raw);
+    } catch (ex) {
+      this.emit("error", ex);
+      return;
+    }
+
+    let market = this._candleSubs.get(msg.marketSymbol);
+    if (!market) return;
+
+    let candle = this._constructCandle(msg.delta, market);
+    this.emit("candle", candle, market);
   }
 
-  // prettier-ignore
+  _constructCandle(msg) {
+    return new Candle(
+      moment.utc(msg.startsAt).valueOf(),
+      msg.open,
+      msg.high,
+      msg.low,
+      msg.close,
+      msg.volume
+    );
+  }
+
+  /**
+   {
+      marketSymbol: 'BTC-USDT',
+      depth: 500,
+      sequence: 545851,
+      bidDeltas: [
+        { quantity: '0', rate: '12338.47320003' },
+        { quantity: '0.01654433', rate: '10800.62000000' }
+      ],
+      askDeltas: []
+    }
+   */
+  _processLevel2Update(err, raw) {
+    if (err) {
+      this.emit("error", err);
+      return;
+    }
+
+    let msg;
+    try {
+      msg = JSON.parse(raw);
+    } catch (ex) {
+      this.emit("error", ex);
+      return;
+    }
+
+    let market = this._level2UpdateSubs.get(msg.marketSymbol);
+    if (!market) return;
+
+    let update = this._constructLevel2Update(msg, market);
+    this.emit("l2update", update, market);
+  }
+
   _constructLevel2Update(msg, market) {
-    let sequenceId = msg.Nonce;
-    let bids = msg.Buys.map(p => new Level2Point(p.Rate.toFixed(8), p.Quantity.toFixed(8), undefined, { type: p.Type }));
-    let asks = msg.Sells.map(p => new Level2Point(p.Rate.toFixed(8), p.Quantity.toFixed(8), undefined, { type: p.Type }));
+    let sequenceId = msg.sequence;
+    let depth = msg.depth;
+    let bids = msg.bidDeltas.map(p => new Level2Point(p.rate, p.quantity, undefined, { depth }));
+    let asks = msg.askDeltas.map(p => new Level2Point(p.rate, p.quantity, undefined, { depth }));
     return new Level2Update({
-      exchange: "Bittrex",
+      exchange: this._name,
       base: market.base,
       quote: market.quote,
       sequenceId,
@@ -350,17 +496,18 @@ class BittrexClient extends EventEmitter {
       bids,
     });
   }
+}
 
-  _getTradeId(msg) {
-    let ms = moment.utc(msg.TimeStamp).valueOf();
-    let buysell = msg.OrderType === "BUY" ? 1 : 0;
-    let price = msg.Rate.toFixed(8);
-    let amount = msg.Quantity.toFixed(8);
-    let preimage = `${ms}:${buysell}:${price}:${amount}`;
-    let hasher = crypto.createHash("md5");
-    hasher.update(preimage);
-    let tradeId = hasher.digest().toString("hex");
-    return tradeId;
+function candlePeriod(period) {
+  switch (period) {
+    case CandlePeriod._1m:
+      return "MINUTE_1";
+    case CandlePeriod._5m:
+      return "MINUTE_5";
+    case CandlePeriod._1h:
+      return "HOUR_1";
+    case CandlePeriod._1d:
+      return "DAY_1";
   }
 }
 
