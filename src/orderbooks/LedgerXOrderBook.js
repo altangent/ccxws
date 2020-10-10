30e4b277419ba3642fbdd84e6a2cbba2c5664fa7
bmancini55 <bmancini@gmail.com>
2020-10-09 22:13:51 -0400

    erisx: adding order book

--- src/orderbooks/LedgerXOrderBook.js
+++ src/orderbooks/LedgerXOrderBook.js
@@ -1,22 +1,22 @@
 const { L3Point } = require("./L3Point");
-const KrakenOrderBookPoint = require("./KrakenOrderBookPoint");
+const { L3PointStore } = require("./L3PointStore");
 
 /**
  * Maintains a Level 3 order book for LedgerX
  */
 class LedgerXOrderBook {
   constructor(snap) {
-    this.asks = new Map();
-    this.bids = new Map();
+    this.asks = new L3PointStore();
+    this.bids = new L3PointStore();
     this.sequenceId = snap.sequenceId;
     this.runId = 0;
 
     for (let ask of snap.asks) {
-      this.asks.set(ask.orderId, new L3Point(ask.orderId, Number(ask.price), Number(ask.size)));
+      this.asks.set(new L3Point(ask.orderId, Number(ask.price), Number(ask.size)));
     }
 
     for (let bid of snap.bids) {
-      this.bids.set(bid.orderId, new L3Point(bid.orderId, Number(bid.price), Number(bid.size)));
+      this.bids.set(new L3Point(bid.orderId, Number(bid.price), Number(bid.size)));
     }
   }
 
@@ -26,14 +26,10 @@ class LedgerXOrderBook {
     this.bids.clear();
   }
 
-  /**
-   *
-   * @param {L3Point} updatea
-   */
   update(update) {
     this.sequenceId += 1;
 
-    // Capature the runId of the first update
+    // Capture the runId of the first update
     if (this.runId === 0) {
       this.runId = update.runId;
     }
@@ -76,7 +72,7 @@ class LedgerXOrderBook {
 
     // Insert the new point
     else {
-      map.set(orderId, new L3Point(orderId, price, size, timestamp));
+      map.set(new L3Point(orderId, price, size, timestamp));
     }
   }
 
@@ -88,48 +84,10 @@ class LedgerXOrderBook {
     return {
       sequenceId: this.sequenceId,
       runId: this.runId,
-      asks: snapSide(this.asks, sortAsc, depth),
-      bids: snapSide(this.bids, sortDesc, depth),
+      asks: this.asks.snapshot(depth, "asc"),
+      bids: this.bids.snapshot(depth, "desc"),
     };
   }
-}
-
-function snapSide(map, sorter, depth) {
-  const aggMap = aggByPrice(map);
-  return Array.from(aggMap.values())
-    .sort(sorter)
-    .slice(0, depth);
-}
-
-function aggByPrice(map) {
-  // Aggregate the values into price points
-  const aggMap = new Map();
-  for (const point of map.values()) {
-    const price = Number(point.price);
-    const size = Number(point.size);
-
-    // If we don't have this price point in the aggregate then we create
-    // a new price point with empty values.
-    if (!aggMap.has(price)) {
-      aggMap.set(price, new KrakenOrderBookPoint(price, 0, 0));
-    }
-
-    // Obtain the price point from the aggregation
-    const aggPoint = aggMap.get(price);
-
-    // Update the size
-    aggPoint.size += size;
-  }
-
-  return aggMap;
-}
-
-function sortAsc(a, b) {
-  return a.price - b.price;
-}
-
-function sortDesc(a, b) {
-  return b.price - a.price;
 }
 
 module.exports = LedgerXOrderBook;
