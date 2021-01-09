b7f04995de0324c9efbbb3e4001bbb02490486c3
Ruslan Kyslyi <ruslan.kyslyi@andela.com>
2021-01-09 13:29:38 +0200

    feat: binance testnet property

--- src/exchanges/binance-client.js
+++ src/exchanges/binance-client.js
@@ -7,14 +7,22 @@ class BinanceClient extends BinanceBase {
     socketBatchSize = 200,
     socketThrottleMs = 1000,
     restThrottleMs = 1000,
+    testNet = false,
     watcherMs,
     l2updateSpeed,
     l2snapshotSpeed,
   } = {}) {
+    let wssPath = "wss://stream.binance.com:9443/stream";
+    let apiPath = "https://api.binance.com/api";
+    if (testNet) {
+      wssPath = "wss://testnet.binance.vision/stream";
+      apiPath = "https://testnet.binance.vision/api"
+    }
+
     super({
       name: "Binance",
-      wssPath: "wss://stream.binance.com:9443/stream",
-      restL2SnapshotPath: "https://api.binance.com/api/v1/depth",
+      restL2SnapshotPath: `${apiPath}/v1/depth`,
+      wssPath,
       useAggTrades,
       requestSnapshot,
       socketBatchSize,
