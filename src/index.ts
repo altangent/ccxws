146f5b816989ad1dad0c45f3c6d2b22da0fff27f
bmancini55 <bmancini@gmail.com>
2021-08-09 14:18:47 -0400

    adding default export for backwards compat

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,159 @@
-// export core types
-export { BasicClient } from "./BasicClient";
-export { BasicMultiClient } from "./BasicMultiClient";
-export { SmartWss } from "./SmartWss";
-export { Watcher } from "./Watcher";
+import { BasicClient } from "./BasicClient";
+import { BasicMultiClient } from "./BasicMultiClient";
+import { SmartWss } from "./SmartWss";
+import { Watcher } from "./Watcher";
 
-// export event types
-export { Auction } from "./Auction";
-export { BlockTrade } from "./BlockTrade";
-export { Candle } from "./Candle";
-export { CandlePeriod } from "./CandlePeriod";
-export { Level2Point } from "./Level2Point";
-export { Level2Snapshot } from "./Level2Snapshots";
-export { Level2Update } from "./Level2Update";
-export { Level3Point } from "./Level3Point";
-export { Level3Snapshot } from "./Level3Snapshot";
-export { Level3Update } from "./Level3Update";
-export { Ticker } from "./Ticker";
-export { Trade } from "./Trade";
+import { Auction } from "./Auction";
+import { BlockTrade } from "./BlockTrade";
+import { Candle } from "./Candle";
+import { CandlePeriod } from "./CandlePeriod";
+import { Level2Point } from "./Level2Point";
+import { Level2Snapshot } from "./Level2Snapshots";
+import { Level2Update } from "./Level2Update";
+import { Level3Point } from "./Level3Point";
+import { Level3Snapshot } from "./Level3Snapshot";
+import { Level3Update } from "./Level3Update";
+import { Ticker } from "./Ticker";
+import { Trade } from "./Trade";
 
-// exportclients
-export { BiboxClient, BiboxClient as Bibox } from "./exchanges/BiboxClient";
-export { BinanceClient, BinanceClient as Binance } from "./exchanges/BinanceClient";
-export { BinanceFuturesCoinmClient, BinanceFuturesCoinmClient as BinanceFuturesCoinM } from "./exchanges/BinanceFuturesCoinmClient"; // prettier-ignore
-export { BinanceFuturesUsdtmClient, BinanceFuturesUsdtmClient as BinanceFuturesUsdtM } from "./exchanges/BinanceFuturesUsdtmClient"; // prettier-ignore
-export { BinanceJeClient, BinanceJeClient as BinanceJe } from "./exchanges/BinanceJeClient";
-export { BinanceUsClient, BinanceUsClient as BinanceUs } from "./exchanges/BinanceUsClient";
-export { BitfinexClient, BitfinexClient as Bitfinex } from "./exchanges/BitfinexClient";
-export { BitflyerClient, BitflyerClient as Bitflyer } from "./exchanges/BitflyerClient";
-export { BithumbClient, BithumbClient as Bithumb } from "./exchanges/BithumbClient";
-export { BitmexClient, BitmexClient as BitMEX } from "./exchanges/BitmexClient";
-export { BitstampClient, BitstampClient as Bitstamp } from "./exchanges/BitstampClient";
-export { BittrexClient, BittrexClient as Bittrex } from "./exchanges/BittrexClient";
-export { CexClient, CexClient as Cex } from "./exchanges/CexClient";
-export { CoinbaseProClient, CoinbaseProClient as CoinbasePro } from "./exchanges/CoinbaseProClient";
-export { CoinexClient, CoinexClient as Coinex } from "./exchanges/CoinexClient";
-export { DeribitClient, DeribitClient as Deribit } from "./exchanges/DeribitClient";
-export { DigifinexClient, DigifinexClient as Digifinex } from "./exchanges/DigifinexClient";
-export { ErisXClient, ErisXClient as ErisX } from "./exchanges/ErisxClient";
-export { FtxClient, FtxClient as Ftx } from "./exchanges/FtxClient";
-export { FtxUsClient, FtxUsClient as FtxUs } from "./exchanges/FtxUsClient";
-export { GateioClient, GateioClient as Gateio } from "./exchanges/GateioClient";
-export { GeminiClient, GeminiClient as Gemini } from "./exchanges/Geminiclient";
-export { HitBtcClient, HitBtcClient as HitBTC } from "./exchanges/HitBtcClient";
-export { HuobiClient, HuobiClient as Huobi } from "./exchanges/HuobiClient";
-export { HuobiFuturesClient, HuobiFuturesClient as HuobiFutures } from "./exchanges/HuobiFuturesClient"; // prettier-ignore
-export { HuobiSwapsClient, HuobiSwapsClient as HuobiSwaps } from "./exchanges/HuobiSwapsClient";
-export { HuobiJapanClient, HuobiJapanClient as HuobiJapan } from "./exchanges/HuobiJapanClient";
-export { HuobiKoreaClient, HuobiKoreaClient as HuobiKorea } from "./exchanges/HuobiKoreaClient";
-export { KucoinClient, KucoinClient as Kucoin } from "./exchanges/KucoinClient";
-export { KrakenClient, KrakenClient as Kraken } from "./exchanges/KrakenClient";
-export { LedgerXClient, LedgerXClient as LedgerX } from "./exchanges/LedgerXClient";
-export { LiquidClient, LiquidClient as Liquid } from "./exchanges/LiquidClient";
-export { OkexClient as OkexClient, OkexClient as OKEx } from "./exchanges/OkexClient";
-export { PoloniexClient, PoloniexClient as Poloniex } from "./exchanges/PoloniexClient";
-export { UpbitClient, UpbitClient as Upbit } from "./exchanges/UpbitClient";
-export { ZbClient, ZbClient as Zb } from "./exchanges/ZbClient";
+import { BiboxClient } from "./exchanges/BiboxClient";
+import { BinanceClient } from "./exchanges/BinanceClient";
+import { BinanceFuturesCoinmClient } from "./exchanges/BinanceFuturesCoinmClient";
+import { BinanceFuturesUsdtmClient } from "./exchanges/BinanceFuturesUsdtmClient";
+import { BinanceJeClient } from "./exchanges/BinanceJeClient";
+import { BinanceUsClient } from "./exchanges/BinanceUsClient";
+import { BitfinexClient } from "./exchanges/BitfinexClient";
+import { BitflyerClient } from "./exchanges/BitflyerClient";
+import { BithumbClient } from "./exchanges/BithumbClient";
+import { BitmexClient } from "./exchanges/BitmexClient";
+import { BitstampClient } from "./exchanges/BitstampClient";
+import { BittrexClient } from "./exchanges/BittrexClient";
+import { CexClient } from "./exchanges/CexClient";
+import { CoinbaseProClient } from "./exchanges/CoinbaseProClient";
+import { CoinexClient } from "./exchanges/CoinexClient";
+import { DeribitClient } from "./exchanges/DeribitClient";
+import { DigifinexClient } from "./exchanges/DigifinexClient";
+import { ErisXClient } from "./exchanges/ErisxClient";
+import { FtxClient } from "./exchanges/FtxClient";
+import { FtxUsClient } from "./exchanges/FtxUsClient";
+import { GateioClient } from "./exchanges/GateioClient";
+import { GeminiClient } from "./exchanges/Geminiclient";
+import { HitBtcClient } from "./exchanges/HitBtcClient";
+import { HuobiClient } from "./exchanges/HuobiClient";
+import { HuobiFuturesClient } from "./exchanges/HuobiFuturesClient";
+import { HuobiJapanClient } from "./exchanges/HuobiJapanClient";
+import { HuobiKoreaClient } from "./exchanges/HuobiKoreaClient";
+import { HuobiSwapsClient } from "./exchanges/HuobiSwapsClient";
+import { KrakenClient } from "./exchanges/KrakenClient";
+import { KucoinClient } from "./exchanges/KucoinClient";
+import { LedgerXClient } from "./exchanges/LedgerXClient";
+import { LiquidClient } from "./exchanges/LiquidClient";
+import { OkexClient } from "./exchanges/OkexClient";
+import { PoloniexClient } from "./exchanges/PoloniexClient";
+import { UpbitClient } from "./exchanges/UpbitClient";
+import { ZbClient } from "./exchanges/ZbClient";
+
+export {
+    //
+    // Base clients
+    BasicClient,
+    BasicMultiClient,
+    SmartWss,
+    Watcher,
+    //
+    // Event types
+    Auction,
+    BlockTrade,
+    Candle,
+    CandlePeriod,
+    Level2Point,
+    Level2Snapshot,
+    Level2Update,
+    Level3Point,
+    Level3Snapshot,
+    Level3Update,
+    Ticker,
+    Trade,
+    //
+    // Clients
+    BiboxClient,
+    BinanceClient,
+    BinanceFuturesCoinmClient,
+    BinanceFuturesUsdtmClient,
+    BinanceJeClient,
+    BinanceUsClient,
+    BitfinexClient,
+    BitflyerClient,
+    BithumbClient,
+    BitmexClient,
+    BitstampClient,
+    BittrexClient,
+    CexClient,
+    CoinbaseProClient,
+    CoinexClient,
+    DeribitClient,
+    DigifinexClient,
+    ErisXClient,
+    FtxClient,
+    FtxUsClient,
+    GateioClient,
+    GeminiClient,
+    HitBtcClient,
+    HuobiClient,
+    HuobiFuturesClient,
+    HuobiSwapsClient,
+    HuobiJapanClient,
+    HuobiKoreaClient,
+    KucoinClient,
+    KrakenClient,
+    LedgerXClient,
+    LiquidClient,
+    OkexClient,
+    PoloniexClient,
+    UpbitClient,
+    ZbClient,
+};
+
+/**
+ * @deprecated Use named imports instead of default import. Client
+ * names have also changed and are now suffixed with `Client`. Deprecation
+ * warning added in v0.46.0 and will be removed in a future version.
+ */
+export default {
+    Bibox: BiboxClient,
+    Binance: BinanceClient,
+    BinanceFuturesCoinM: BinanceFuturesCoinmClient,
+    BinanceFuturesUsdtM: BinanceFuturesUsdtmClient,
+    BinanceJe: BinanceJeClient,
+    BinanceUs: BinanceUsClient,
+    Bitfinex: BitfinexClient,
+    Bitflyer: BitflyerClient,
+    Bithumb: BithumbClient,
+    BitMEX: BitmexClient,
+    Bitstamp: BitstampClient,
+    Bittrex: BittrexClient,
+    Cex: CexClient,
+    CoinbasePro: CoinbaseProClient,
+    Coinex: CoinexClient,
+    Deribit: DeribitClient,
+    Digifinex: DigifinexClient,
+    ErisX: ErisXClient,
+    Ftx: FtxClient,
+    FtxUs: FtxUsClient,
+    Gateio: GateioClient,
+    Gemini: GeminiClient,
+    HitBTC: HitBtcClient,
+    Huobi: HuobiClient,
+    HuobiFutures: HuobiFuturesClient,
+    HuobiSwaps: HuobiSwapsClient,
+    HuobiJapan: HuobiJapanClient,
+    HuobiKorea: HuobiKoreaClient,
+    Kucoin: KucoinClient,
+    Kraken: KrakenClient,
+    LedgerX: LedgerXClient,
+    Liquid: LiquidClient,
+    OKEx: OkexClient,
+    Poloniex: PoloniexClient,
+    Upbit: UpbitClient,
+    Zb: ZbClient,
+};
